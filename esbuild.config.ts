d33e5f9333b614dae0d1b289ec1d35e53da6bc4c
ras0q <ras0q@users.noreply.github.com>
2024-10-18 08:56:06 +0900

    migrate to Deno

---
+++ esbuild.config.ts
@@ -0,0 +1,37 @@
+import esbuild from "esbuild";
+
+const prod = Deno.args[0] === "production";
+
+const context = await esbuild.context({
+  entryPoints: ["main.ts"],
+  bundle: true,
+  external: [
+    "obsidian",
+    "electron",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/highlight",
+    "@lezer/lr",
+  ],
+  format: "cjs",
+  target: "es2018",
+  logLevel: "info",
+  sourcemap: prod ? false : "inline",
+  treeShaking: true,
+  outfile: "main.js",
+  minify: prod,
+});
+
+if (prod) {
+  await context.rebuild();
+  Deno.exit(0);
+} else {
+  await context.watch();
+}
